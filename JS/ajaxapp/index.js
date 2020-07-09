f1e3381dc2feb13612b6146dc0fb6e08c1c86cb1
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-07-09 20:36:05 +0900

    画面にユーザー情報を表示

--- JS/ajaxapp/index.js
+++ JS/ajaxapp/index.js
@@ -12,11 +12,42 @@ function fetchUserInfo(userId) {
         // else コンソールにユーザー情報を表示
         console.log('成功')
         return response.json().then(userInfo => {
-          console.log(userInfo);
+          const view = escapeHTML`
+          <h4>${userInfo.name} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+              <dt>Location</dt>
+              <dd>${userInfo.location}</dd>
+              <dt>Repositories</dt>
+              <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+          const result = document.getElementById('result');
+          result.innerHTML = view;
         })
       }
     }).catch(error => {
       // catch: Rejected: ネットワークエラー
       console.error(error)
     })
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 }
