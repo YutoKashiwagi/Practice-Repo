948d37545b145277bb26ba8d65f089d1f67cf165
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-07-09 21:25:52 +0900

    関数の分割、エラーハンドリング

--- JS/ajaxapp/index.js
+++ JS/ajaxapp/index.js
@@ -1,3 +1,11 @@
+function main() {
+  fetchUserInfo('js-primer-example')
+    .catch((error) => {
+      // Promiseチェーンの中で発生したエラーを受け取る
+      console.error('エラーが発生しました');
+    })
+}
+
 function fetchUserInfo(userId) {
   // getリクエストを送る
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
@@ -5,31 +13,38 @@ function fetchUserInfo(userId) {
     .then(response => {
       console.log(response.status)
       if (!response.ok) {
-        // if 失敗時の処理: コンソールにログを出力
-        // レスポンスの表示
-        console.error('エラーレスポンス', response);
+        // エラーレスポンスからRejectedなPromiseを作成して返す
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
       } else {
         // else コンソールにユーザー情報を表示
         console.log('成功')
         return response.json().then(userInfo => {
-          const view = escapeHTML`
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
-          const result = document.getElementById('result');
-          result.innerHTML = view;
+          const view = createView(userInfo);
+          displayView(view);
         })
       }
     }).catch(error => {
       // catch: Rejected: ネットワークエラー
       console.error(error)
     })
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
