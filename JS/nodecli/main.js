666019c0b8d9aae2f423cc4cc2120cc4bd9c54e0
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-07-10 20:38:30 +0900

    gfmオプション追加

--- JS/nodecli/main.js
+++ JS/nodecli/main.js
@@ -5,10 +5,18 @@ const fs = require("fs");
 // markedモジュールをインポート
 const marked = require("marked");
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数をパース
 program.parse(process.argv);
 // ファイルパスを配列から取り出す
 const filepPath = program.args[0];
+
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
 
 // ファイルを非同期で読み込む
 fs.readFile(filepPath, { encoding: "utf8" }, (err, file) => {
@@ -19,7 +27,10 @@ fs.readFile(filepPath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file);
+  const html = marked(file, {
+    // オプションの値を使用する
+    gfm: cliOptions.gfm,
+  });
   console.log(html);
 }
 );
