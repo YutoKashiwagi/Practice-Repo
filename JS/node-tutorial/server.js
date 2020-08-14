80eb0647613d197cf1442bafd21aa59f869980f6
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-08-14 16:53:25 +0900

    ノンブロッキング操作で応答するリクエストハンドラ

--- JS/node-tutorial/server.js
+++ JS/node-tutorial/server.js
@@ -1,16 +1,12 @@
 var http = require("http");
-var url = require('url');
-const { route } = require("./router");
+var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log(`Request for ${pathname} received.`);
+    console.log("Request for " + pathname + " received.");
 
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    content = route(handle, pathname)
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
